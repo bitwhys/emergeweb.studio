d4995138e4805ed2cade4edeafb3bf558c764fbb
George Bockari <bitwhys@gmail.com>
2022-10-05 20:30:07 -0400

    refactor: add custom favicons

--- js/plugins.js
+++ js/plugins.js
@@ -1,39 +1,27 @@
-
 // remap jQuery to $
-(function($){
-
- 
-
-
-
-
-
- 
-
+(function ($) {
 
 
 })(this.jQuery);
 
 
-
 // usage: log('inside coolFunc',this,arguments);
 // paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
-window.log = function(){
+window.log = function () {
   log.history = log.history || [];   // store logs to an array for reference
   log.history.push(arguments);
-  if(this.console){
-    console.log( Array.prototype.slice.call(arguments) );
+  if (this.console) {
+    console.log(Array.prototype.slice.call(arguments));
   }
 };
 
 
-
 // catch all document.write() calls
-(function(doc){
+(function (doc) {
   var write = doc.write;
-  doc.write = function(q){ 
-    log('document.write(): ',arguments); 
-    if (/docwriteregexwhitelist/.test(q)) write.apply(doc,arguments);  
+  doc.write = function (q) {
+    log('document.write(): ', arguments);
+    if (/docwriteregexwhitelist/.test(q)) write.apply(doc, arguments);
   };
 })(document);
 
